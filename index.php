1fe8b45a8830ed6e3b4bc05c688d628aabd561e6
Bijay Subba Limbu <bijaysubbalimbu@gmail.com>
2023-04-06 17:59:18 +0545

    add images

--- index.php
+++ index.php
@@ -881,18 +881,7 @@
     
     <!--====== APPIE TESTIMONIAL PART ENDS ====== -->
 
-    <section class="appie-project-3-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="appie-project-3-box d-block d-md-flex justify-content-between align-items-center wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="400ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 400ms; animation-name: fadeInUp;">
-                        <h4 class="title">Start your project <br> with Appie.</h4>
-                        <a class="main-btn" href="#">Let’s Talk</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    
 
     <!--====== APPIE FOOTER PART START ======-->
     
